467329202af7c382b301aa4c76d8288d3de31dec
Kristen <43305741+KISMET-INC@users.noreply.github.com>
2020-12-24 14:21:50 -0800

    renaming the project

--- pets_connect/wsgi.py
+++ pets_connect/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'user_dashboard_two.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pets_connect.settings')
 
 application = get_wsgi_application()
