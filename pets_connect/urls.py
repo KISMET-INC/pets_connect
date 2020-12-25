5d6196f3fa1da99ed398cdb35c41ba0f7aa731b8
Kristen <43305741+KISMET-INC@users.noreply.github.com>
2020-12-25 05:10:20 -0800

    messing with debug

--- pets_connect/urls.py
+++ pets_connect/urls.py
@@ -23,7 +23,4 @@ urlpatterns = [
 path('', include('app_one.urls')),
 ]
 
-
-if settings.DEBUG:
-        urlpatterns += static(settings.MEDIA_URL,
-                              document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
