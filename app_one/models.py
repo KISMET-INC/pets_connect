8a67595c6fa7f4495b99358fc36550722e0f83ca
Kristen <43305741+KISMET-INC@users.noreply.github.com>
2021-03-08 10:14:54 -0800

    profile share pet to owner name, name validation

--- app_one/models.py
+++ app_one/models.py
@@ -136,7 +136,10 @@ class PetManager(models.Manager):
 
         if len(post['desc']) < 2:
             errors['desc'] ='Please tell us YOUR name too.'
-        
+
+        if len(post['desc']) > 10 or len(post['name']) > 10:
+            errors['desc'] ='Sorry, names must be between 2 and 10 characters'
+
         if 'pet_img' not in postFiles:
             errors['image'] = 'An image is required.' 
 
