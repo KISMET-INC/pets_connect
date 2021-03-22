b16db185f04a95ecedc637c165499120b73d5e49
Kristen <43305741+KISMET-INC@users.noreply.github.com>
2021-03-21 19:52:58 -0700

    add share to bulletin

--- app_one/views.py
+++ app_one/views.py
@@ -349,6 +349,7 @@ def bulletin(request):
         # 'image': current_image,
         'images': Image.objects.order_by("-updated_at"),
         'users': User.objects.order_by("updated_at"),
+        'upload_pet_form': UploadPetForm(),
         # 'comments': Comment.objects.filter(image = current_image).order_by('-created_at'),
     }
     return render(request,'bulletin.html',context)
@@ -374,14 +375,14 @@ def process_add_pet_image(request):
     errors = Image.objects.basic_validator_add_pet(request.POST, request.FILES)
     session_user = User.objects.get(id=request.session['user_id'])
     user = User.objects.get(id=request.POST['user_id'])
-
+    print(f'errors: {len(errors)}')
     if len(errors) > 0:
         for value in errors.values():
             messages.error(request,value)
         if session_user.user_level == 9:
             return redirect (f'/admin_edit_user/{user.id}')
-        if request.POST['location'] == 'explore':
-            return redirect('/explore')
+        if 'location' in request.POST:
+            return redirect(f'/{request.POST["location"]}')
 
     upload_pet_form = UploadPetForm(request.POST, request.FILES)
     this_image = Image.objects.create(pet_img = request.FILES['pet_img'], user = user, name = request.POST['name'], desc = request.POST['desc'] )
@@ -389,8 +390,8 @@ def process_add_pet_image(request):
     send_email(session_user = user, action = 'SHARED', image = this_image)
     if session_user.user_level == 9:
         return redirect (f'/admin_edit_user/{user.id}')
-    if request.POST['location'] == 'explore':
-        return redirect('/explore')
+    if 'location' in request.POST:
+        return redirect(f'/{request.POST["location"]}')
 
     return redirect (f'/profile/{user.id}')
 
